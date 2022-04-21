06fd4fd40635f6caa8de248079b4be3fd93a406b
placidcy <97069998+placidcy@users.noreply.github.com>
2022-04-21 19:23:17 +0900

    11
    111

---
+++ gameTest/game/musicPlayer/api/music.js
@@ -0,0 +1,121 @@
+let allMusic = [
+    {
+        name : "1. Harley Bird - Home",
+        artist : "Jone Mickal",
+        img : "music-1",
+        audio : "music-1"
+    },{
+        name : "2. Ikson Anywhere – Ikson",
+        artist : "Jone Mickal",
+        img : "music-2",
+        audio : "music-2"
+    },
+    {
+        name : "3. Jim - Sun Goes Down",
+        artist : "Jone Mickal",
+        img : "music-3",
+        audio : "music-3"
+    },
+    {
+        name : "4. RUNRUN (feat. Skinny Brown)",
+        artist : "Jone Mickal",
+        img : "music-4",
+        audio : "music-4"
+    },
+    {
+        name : "5. Z Flip Fold (Feat. Damndef) - remix",
+        artist : "Jone Mickal",
+        img : "music-5",
+        audio : "music-5"
+    },
+    {
+        name : "6. 뚝뚝 (Feat. Chaboom, 차메인, Don Mills)",
+        artist : "Jone Mickal",
+        img : "music-6",
+        audio : "music-6"
+    },
+    {
+        name : "7. 담금주 (Feat. Chaboom)",
+        artist : "Jone Mickal",
+        img : "music-7",
+        audio : "music-7"
+    },
+    {
+        name : "8. thisisneverthat - HD BL4CK remix",
+        artist : "Jone Mickal",
+        img : "music-8",
+        audio : "music-8"
+    },
+    {
+        name : "9. We Are 우리는",
+        artist : "Jone Mickal",
+        img : "music-9",
+        audio : "music-9"
+    },
+    {
+        name : "10. Laugh Like A Sheep And",
+        artist : "Jone Mickal",
+        img : "music-10",
+        audio : "music-10"
+    },
+    {
+        name : "11. Move Like A Snake 양처럼 웃고 뱀처럼 벌어",
+        artist : "Jone Mickal",
+        img : "music-11",
+        audio : "music-11"
+    },
+    {
+        name : "12. Chef",
+        artist :  "Jone Mickal",
+        img : "music-12",
+        audio : "music-12"
+    },
+    {
+        name : "13. RUNRUN (feat. Skinny Brown)",
+        artist : "Jone Mickal",
+        img : "music-13",
+        audio : "music-13"
+    },
+    {
+        name : "14. Grave Freestyle (feat. kaogaii)",
+        artist :  "Jone Mickal",
+        img : "music-14",
+        audio : "music-14"
+    },
+    {
+        name : "15. Chase the money Never chase a bitch",
+        artist : "Jone Mickal",
+        img : "music-15",
+        audio : "music-15"
+    },
+    {
+        name : "16. money Never",
+        artist : "Jone Mickal",
+        img : "music-16",
+        audio : "music-16"
+    },
+    {
+        name : "17. Never chase a bitch",
+        artist : "Jone Mickal",
+        img : "music-17",
+        audio : "music-17"
+    },
+    {
+        name : "18. Never Sober (feat. Cloudybay)",
+        artist : "Jone Mickal",
+        img : "music-18",
+        audio : "music-18"
+    },
+    {
+        name : "19. HICKEY!",
+        artist : "Jone Mickal",
+        img : "music-19",
+        audio : "music-19"
+    },
+    {
+        name : "20. FREE ME (feat. Lil 9ap)",
+        artist : "Jone Mickal",
+        img : "music-20",
+        audio : "music-20"
+    },
+]
